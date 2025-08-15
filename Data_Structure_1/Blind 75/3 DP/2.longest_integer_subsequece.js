64d33e50afbeef3c32fd3315b6c1800e576e41bb
Amit Paliwal <amitpaliwal259@gmail.com>
2025-08-15 23:21:19 +0530

    added new

---
+++ Data_Structure_1/Blind 75/3 DP/2.longest_integer_subsequece.js
@@ -0,0 +1,62 @@
+/*
+300. Longest Increasing Subsequence
+
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+ 
+
+*/
+
+var lengthOfLIS = function(nums) {
+    const res = [];
+
+    const binarySearch = (arr, target) => {
+        let left = 0;
+        let right = arr.length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (arr[mid] === target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    };
+
+    for (const n of nums) {
+        if (!res.length || res[res.length - 1] < n) {
+            res.push(n);
+        } else {
+            const idx = binarySearch(res, n);
+            res[idx] = n;
+        }
+    }
+
+    return res.length;    
+};
