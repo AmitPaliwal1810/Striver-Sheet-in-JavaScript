e4265615381006ab9282b2d545b47dadc0bf8405
Amit Paliwal <amitpaliwal259@gmail.com>
2025-08-24 17:09:28 +0530

    2nd way

--- Data_Structure_1/Blind 75/3 DP/3.longest_common_subsequece.js
+++ Data_Structure_1/Blind 75/3 DP/3.longest_common_subsequece.js
@@ -57,3 +57,28 @@ let text1 = "abcdaf";
 let text2 = "acbcf";
 
 console.log({ value: longestCommonSubsequence(text1, text2) });
+ 
+
+// 2nd way to write
+
+var longestCommonSubsequence = function(text1, text2) {
+    const m = text1.length;
+    const n = text2.length;
+
+    // Step 1: Create DP table
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    // Step 2: Fill table
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // Step 3: Answer is at bottom-right
+    return dp[m][n];
+};
